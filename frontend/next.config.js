1426d7e7160e59d0fed2ae9ab4c8da66a0b4a54a
AbuMusab-AlZarqawi <adilpedro59@email.com>
2026-05-15 11:59:18 +0100

    Fix async storage build error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,17 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.externals.push(
-      "pino-pretty",
-      "lokijs",
-      "encoding",
-      "@react-native-async-storage/async-storage"
-    );
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
+    config.resolve.fallback = {
+      ...(config.resolve.fallback || {}),
       "@react-native-async-storage/async-storage": false,
     };
+    config.plugins = config.plugins || [];
+    const webpack = require("webpack");
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(
+        /@react-native-async-storage\/async-storage/,
+        require.resolve("./src/lib/asyncStorageMock.js")
+      )
+    );
     return config;
   },
 };
